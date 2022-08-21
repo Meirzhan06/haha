6c7ae7ed090af9f275ad4537c4529ed101bfc192
Anonim <103999938+Meirzhan06@users.noreply.github.com>
2022-08-22 00:15:57 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+$(window).on('load', function() {
+    $preloader = $('.preloader'),
+    $loader = $preloader.find('.preloader');
+    $loader.fadeOut();
+    $preloader.delay(1500).fadeOut('slow');
+  });
+  
 $(document).ready(function(){
     $('.header_burger').click(function(event){
         $('.header_burger,.header_menu').toggleClass('active');
